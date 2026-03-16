3272b8c86015230a457e3a84e19c5d1badf6e029
Unai387 <umeneses@elpuig.xeill.net>
2026-03-16 15:53:23 +0100

    Level2

--- scenes/objects/one_way_platform.gd
+++ scenes/objects/one_way_platform.gd
@@ -1,5 +1,12 @@
-extends StaticBody2D
+extends AnimatableBody2D
 
 func _ready():
+	# Configurar como plataforma one-way
 	collision_layer = 1
 	collision_mask = 0
+	
+	# Habilitar one-way collision manualmente
+	for child in get_children():
+		if child is CollisionShape2D:
+			child.one_way_collision = true
+			child.one_way_collision_margin = 1.0
